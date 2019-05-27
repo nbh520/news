3230c7cc003f2de8c6de27b669ac7d8ce3cd1e8c
nibohan <154524331@qq.com>
2019-05-27 12:45:12 +0800

    0527 更新readme

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:4001"'
+  // BASE_API: '"http://localhost:4001"'
+  BASE_API: '"http://39.108.182.105:4001"'
+
 })
